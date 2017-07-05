867d4ec60f05e3d0b29c99261d3c073ff86a8076
navarrets <javi.navarrete@gmail.com>
2017-07-05 13:00:18 +0200

    Metiendo "Amigo en index"

--- index.php
+++ index.php
@@ -5,5 +5,5 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-echo "Que hay de nuevo amigo??";
+echo "Que hay de nuevo amigo del alma??";
 ?>
